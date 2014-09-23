6f6f59c5febbc6948eb9b1bd6da1f3b902e9e0ef
Hana Kim <hanakimis@gmail.com>
2014-09-23 01:00:25 -0700

    empty project

---
+++ Group Project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Group Project
+//
+//  Created by Hana Kim on 9/23/14.
+//  Copyright (c) 2014 Hana-Ida-Jessica. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
