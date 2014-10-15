62546412eb9a978b57a2dcd3c5fa1c3ab34e91dd
Hana Kim <hanakimis@gmail.com>
2014-10-14 17:56:07 -0700

    adding keyboard stuff

---
+++ Group Project/SettingsViewController.swift
@@ -0,0 +1,87 @@
+//
+//  SettingsViewController.swift
+//  Group Project
+//
+//  Created by Hana Kim on 10/14/14.
+//  Copyright (c) 2014 Hana-Ida-Jessica. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController, UITextViewDelegate {
+
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var profileView: UIView!
+    @IBOutlet weak var passwordView: UIView!
+    @IBOutlet weak var oldPasswordTextField: UITextField!
+    @IBOutlet weak var newPasswordTextField: UITextField!
+    @IBOutlet weak var confirmPasswordTextField: UITextField!
+    
+    var selectedTextField: Int!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        nameTextField.tag  = 0
+        emailTextField.tag = 1
+        
+        oldPasswordTextField.tag     = 2
+        newPasswordTextField.tag     = 3
+        confirmPasswordTextField.tag = 4
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
+    }
+
+    func keyboardWillShow(notification: NSNotification!) {
+        var userInfo = notification.userInfo!
+        
+        var kbSize = (userInfo[UIKeyboardFrameEndUserInfoKey] as NSValue).CGRectValue().size
+        var durationValue = userInfo[UIKeyboardAnimationDurationUserInfoKey] as NSNumber
+        var animationDuration = durationValue.doubleValue
+        var curveValue = userInfo[UIKeyboardAnimationCurveUserInfoKey] as NSNumber
+        var animationCurve = curveValue.integerValue
+        
+        
+        UIView.animateWithDuration(animationDuration, delay: 0.0, options: UIViewAnimationOptions.fromRaw(UInt(animationCurve << 16))!, animations: {
+                if (self.selectedTextField <= 1) {
+                    self.passwordView.alpha = 0
+                } else {
+                    self.profileView.alpha = 0
+                    self.passwordView.frame.origin = CGPoint(x: 0.0, y: 74.0)
+                }
+            }, completion: nil)
+    }
+    
+    func keyboardWillHide(notification: NSNotification!) {
+        var userInfo = notification.userInfo!
+        
+        var kbSize = (userInfo[UIKeyboardFrameEndUserInfoKey] as NSValue).CGRectValue().size
+        var durationValue = userInfo[UIKeyboardAnimationDurationUserInfoKey] as NSNumber
+        var animationDuration = durationValue.doubleValue
+        var curveValue = userInfo[UIKeyboardAnimationCurveUserInfoKey] as NSNumber
+        var animationCurve = curveValue.integerValue
+        
+        UIView.animateWithDuration(animationDuration, delay: 0.0, options: UIViewAnimationOptions.fromRaw(UInt(animationCurve << 16))!, animations: {
+            self.nameTextField.frame.origin = CGPoint(x: 0, y: 82)
+            self.emailTextField.frame.origin = CGPoint(x: 0, y: 153)
+            }, completion: nil)
+    }
+    
+    
+    func textViewDidBeginEditing(textView: UITextView) {
+        selectedTextField = textView.tag
+    }
+    
+    
+    @IBAction func onTapView(sender: UITapGestureRecognizer) {
+        self.view.endEditing(true)
+    }
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
