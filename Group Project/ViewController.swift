cbe497cf7818a380db7ba696a9340391a8feca5d
Ida Leung <aydagnuel@gmail.com>
2014-10-15 18:25:10 -0700

    Added bkg color and round corners on panels

--- Group Project/ViewController.swift
+++ Group Project/ViewController.swift
@@ -10,8 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var eventOne: UIView!
+    @IBOutlet weak var eventTwo: UIView!
+    @IBOutlet weak var eventThree: UIView!
+    @IBOutlet weak var eventFour: UIView!
+    @IBOutlet weak var eventFive: UIView!
+    
+
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        eventOne.layer.cornerRadius = 2.0
+        eventTwo.layer.cornerRadius = 2.0
+        eventThree.layer.cornerRadius = 2.0
+        eventFour.layer.cornerRadius = 2.0
+        eventFive.layer.cornerRadius = 2.0
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
