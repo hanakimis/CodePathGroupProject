0fcc1fc0d35d006f91fdebe5fd2516513bdcf0e5
Ida Leung <aydagnuel@gmail.com>
2014-10-18 12:38:09 -0700

    New User
    -Scroll on new user screen.

--- Group Project/NewUserViewController.swift
+++ Group Project/NewUserViewController.swift
@@ -8,16 +8,21 @@
 
 import UIKit
 
-class NewUserViewController: UIViewController {
+class NewUserViewController: UIViewController, UIScrollViewDelegate {
 
     @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var intro_3: UIImageView!
+    @IBOutlet weak var intro_2: UIImageView!
+    @IBOutlet weak var intro_1: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-       // scrollView.contentSize =
-       //     settingImage.frame.size
+        scrollView.delegate = self
 
+        scrollView.contentSize = CGSize(width: 960, height: 568)
+
+        println("test")
         // Do any additional setup after loading the view.
     }
 
@@ -26,6 +31,21 @@ class NewUserViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func scrollViewDidEndDecelerating(scrollView: UIScrollView!) {
+        // Get the current page based on the scroll offset
+        var page : Int = Int(round(scrollView.contentOffset.x / 320))
+
+        // Set the current page, so the dots will update
+        //pageControl.currentPage = page
+
+//        if (page == 3) {
+//            UIImageView.animateWithDuration(0.5){self.backupButton.alpha = 1}
+//            UIImageView.animateWithDuration(0.5){self.pageControl.alpha = 0}
+//
+//        }
+
+    }
+
 
     /*
     // MARK: - Navigation
